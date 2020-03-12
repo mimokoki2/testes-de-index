f85b19390bb03ee7be67efb44b25e13e963230a2
S0R4 <56172465+mimokoki2@users.noreply.github.com>
2020-03-12 19:55:43 -0300

    Add files via upload

---
+++ confratteria/teste.js
@@ -0,0 +1,16 @@
+function ocultar() {
+  var element = document.getElementById("teste");
+  if(element.classList == 'geral')
+  {
+  	element.classList.add('hide')
+  }else{
+  	element.classList.remove('hide')
+  }
+
+}
+
+
+function mostrar() {
+  var element = document.getElementById("teste");
+
+}
